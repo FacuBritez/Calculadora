2802b2e1cad6a771d36738243f8beb19bdc04db6
FacuBritez <FacundoIvanBritez@gmail.com>
2023-06-06 20:11:12 -0300

    Empezando a crear el constructor y funciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,112 +9,81 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Calculadora';
 
-  valorAnterior:number|null = null;
-  valorActual:number|null = null;
+  displayValorAnterior: HTMLElement | null;
+  displayValorActual: HTMLElement | null;
+  botonesNumeros: NodeListOf<Element>;
+  botonesOperadores: NodeListOf<Element>;
 
-  borrar() {
+  constructor() {
+    this.displayValorAnterior = document.getElementById('valor-anterior');
+    this.displayValorActual = document.getElementById('valor-actual');
+    this.botonesNumeros = document.querySelectorAll('.numero');
+    this.botonesOperadores = document.querySelectorAll('.operador');
 
+    //display = new Display(displayValorAnterior, displayValorActual);
+
+    console.log(this.botonesNumeros)
+
+    this.botonesNumeros.forEach(boton => {
+      boton.addEventListener('click', () => this.display.agregarNumero(boton.innerHTML));
+    });
+
+    console.log(this.botonesOperadores)
+
+    /*
+    this.botonesOperadores.forEach((boton: HTMLElement) => {
+      boton.addEventListener('click', () => this.display.computar(boton.value));
+    });
+    */
   }
 
-  borrarTodo() {
+  valorAnterior: number | null = null;
+  valorActual: number | null = null;
 
-  }
-
-  computar() {
-
-  }
-
-  agregarNumero() {
-
-  }
-
-  calcular() {
-    
-  }
-
-
-/*
-  displayValorAnterior: number = 0;
-  displayValorActual: number = 0;
-  tipoOperacion: string = '';
-
-  Calculadora = {
+  calculadora = {
     sumar(num1: number, num2: number) {
       return num1 + num2;
     },
-  
+
     restar(num1: number, num2: number) {
       return num1 - num2;
     },
-  
+
     dividir(num1: number, num2: number) {
       return num1 / num2;
     },
-  
+
     multiplicar(num1: number, num2: number) {
       return num1 * num2;
     }
   }
 
-  Display = {
 
-    displayValorAnterior: number = 0;
-    displayValorActual: number = 0;
-    tipoOperacion = '';
-    valorActual: number = 0;
 
-    this.displayValorActual = this.displayValorActual,
-    this.displayValorAnterior = displayValorAnterior,
-    this.calculadora = new this.Calculadora(),
-    this.tipoOperacion = undefined,
-    this.valorActual = "",
-    this.valorAnterior = "",
-    this.signos = {
-      sumar: '+',
-      dividir: '%',
-      multiplicar: 'x',
-      restar: '-',
-    },
 
+  display = {
     borrar() {
-      this.valorActual = this.valorActual.toString().slice(0, -1)
-      this.imprimirValores();
+
     },
 
     borrarTodo() {
-      this.valorActual = '';
-      this.valorAnterior = '';
-      this.tipoOperacion = undefined;
-      this.imprimirValores();
+
     },
 
-    computar(tipo:string) {
-      this.tipoOperacion !== 'igual' && this.calcular()
-      this.tipoOperacion = tipo;
-      this.valorAnterior = this.valorActual || this.valorAnterior;
-      this.valorActual = '';
-      this.imprimirValores();
+    computar(operador:string) {
+
     },
 
     agregarNumero(numero:string) {
-      if (numero == '.' && this.valorActual.includes('.')) return
-      this.valorActual = this.valorActual.toString() + numero.toString();
-      this.imprimirValores();
-    },
-
-    imprimirValores() {
-      this.displayValorActual.textContent = this.valorActual;
-      this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+      console.log("agregarNumero() agregado al boton" + numero)
     },
 
     calcular() {
-      const valorAnterior = parseFloat(this.valorAnterior);
-      const valorActual = parseFloat(this.valorActual);
 
-      if (isNaN(valorActual) || isNaN(valorAnterior)) return
-      this.valorActual = this.calculadora[this.tipoOperacion](valorAnterior, valorActual);
     }
   }
-*/
+
+
+
 
 }
